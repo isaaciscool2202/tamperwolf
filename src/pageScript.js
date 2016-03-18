1701203e03fb7437f63f77f812b1140bac2e72fa
Cristian Lupascu <cristi.lupascu@gmail.com>
2016-03-18 21:50:32 +0200

    different script injection mechanism to be able to access window

--- src/pageScript.js
+++ src/pageScript.js
@@ -6,11 +6,15 @@ var run = function (rule) {
   console.log('Tamperwolf :: running script ' + rule.name);
 
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", rule.scriptUrl, true);
-  xhr.onreadystatechange = function() {
+  xhr.open('GET', rule.scriptUrl, true);
+  xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      var resp = eval(xhr.responseText);
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.textContent = xhr.responseText;
+      document.head.appendChild(script);
     }
-  }
+  };
+
   xhr.send();
 };
